49ff5c45fd87af45bf9e418dfdf722a3fb8f36b1
minjongkim <rlaalswhd200@naver.com>
2021-11-21 18:00:03 +0900

    Initial Commit

---
+++ RisingCamp_week1_cloneAppFront/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RisingCamp_week1_cloneAppFront
+//
+//  Created by 김민종 on 2021/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
